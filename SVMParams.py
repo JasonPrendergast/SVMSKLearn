21d93753ecde2e2ff5c1ad8e81f2e09c7f9f431d
Jason Prendergast <32454226+JasonPrendergast@users.noreply.github.com>
2017-10-02 13:18:37 +0100

    Add files via upload

---
+++ SVMParams.py
@@ -0,0 +1,23 @@
+from sklearn import preprocessing, cross_validation, neighbors, svm
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+print(df)
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+
+x = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(x,y,test_size=0.2)
+
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+example_measures = np.array([4,2,1,1,1,2,3,2,1])
+example_measures = example_measures.reshape(1, -1)
+prediction = clf.predict(example_measures)
+print(prediction)
